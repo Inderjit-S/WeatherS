68eaa303347b9df475fe9ceda834b142b90c287b
Singh <inderjit.singh.644@my.csun.edu>
2024-04-19 20:42:16 -0700

    Finished Token and Login Request

--- WeatherS/Controllers/AdminController.cs
+++ WeatherS/Controllers/AdminController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using WeatherS.DTO;
 
 namespace WeatherS.Controllers
 {
@@ -10,10 +12,27 @@ namespace WeatherS.Controllers
     public class AdminController(UserManager<worldCitiesUser> userManager,
         JwtHandler jwtHandler) : ControllerBase
     {
-        [HttpPost]
-        public void Login()
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(LoginRequest loginRequest)
         {
-
+            worldCitiesUser? user = await userManager.FindByNameAsync(loginRequest.Username);
+            if (user == null)
+            {
+                return Unauthorized("Wrong username");
+            }
+            bool success = await userManager.CheckPasswordAsync(user, loginRequest.Password);
+            if (!success)
+            {
+                return Unauthorized("Wrong password");
+            }
+            JwtSecurityToken token = await jwtHandler.GetTokenAsync(user);
+            string jwtString = new JwtSecurityTokenHandler().WriteToken(token);
+            return Ok(new LoginResult
+            {
+                Success = true,
+                Message = "momlovesus",
+                Token = jwtString
+            });
         }
     }
 }
--- WeatherS/obj/Debug/net8.0/WeatherS.AssemblyInfo.cs
+++ WeatherS/obj/Debug/net8.0/WeatherS.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dedcf3f7d32202dfa2e072d4bd29a2bb1edfad6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+469a6ea1d40c32177255a942545cf0e1872c7948")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
